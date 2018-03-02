d645d92c2041116e3b2700ff590a870f99c840ea
chhar9972 <chhar9972@7L1M6Y1.Student.UGDSB.ED>
2018-03-02 13:05:27 -0500

    Made the Gretzky Program

---
+++ src/waynegretzky/WayneGretzky.java
@@ -0,0 +1,18 @@
+/*
+ * Charlie Harpur
+ * Saves variables wG as "Wayne Gretzky" and num as 99 then prints them to the console
+ * 28/02/2018
+ */
+
+package waynegretzky;
+
+public class WayneGretzky
+{
+    public static void main(String[] args)
+    {
+        String name = "Wayne Gretzky";
+        int number = 99;
+        System.out.println (name + " is number " + number + ".");
+    }
+    
+}
